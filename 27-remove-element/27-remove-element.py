df495eb958720a133d413dfacce2081a2b490f23
Husain Ridwan <h.ridwan707@gmail.com>
2022-07-29 18:25:41 +0100

    Time: 46 ms (8.10%), Space: 13.4 MB (63.72%) - LeetHub

--- 27-remove-element/27-remove-element.py
+++ 27-remove-element/27-remove-element.py
@@ -5,15 +5,15 @@ class Solution(object):
         :type val: int
         :rtype: int
         """
-        l = 0
+        """l = 0
         for r in range(len(nums)):
-            if val in nums:
+            if val != nums:
                 nums.remove(val)
             l += 1
-        return l
-        """i = 0
+        return l"""
+        i = 0
         for num in nums:
             if num != val:
                 nums[i] = num
                 i += 1
-        return i"""
+        return i
