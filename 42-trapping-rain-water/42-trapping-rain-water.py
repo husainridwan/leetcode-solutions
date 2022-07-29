904bb3e2ff516470f0eee007a757431e478544df
Husain Ridwan <h.ridwan707@gmail.com>
2022-07-29 01:53:11 +0100

    Time: 205 ms (14.64%), Space: 14.8 MB (95.87%) - LeetHub

---
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        l, r = 0, len(height)-1
+        maxL, maxR, total = 0, 0, 0
+        
+        while(l < r):
+            if height[l] <= height[r]:
+                if height[l] >= maxL:
+                    maxL = height[l]
+                else:
+                    total += maxL - height[l]
+                l += 1
+            else:
+                if height[r] <= height[l]:
+                    if height[r] >= maxR:
+                        maxR = height[r]
+                    else:
+                        total += maxR - height[r]
+                r -= 1
+        return total
+        
