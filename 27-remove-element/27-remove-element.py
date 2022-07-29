7d64d40c413d124616bbfb44a5675ac564350d93
Husain Ridwan <h.ridwan707@gmail.com>
2022-07-29 18:28:57 +0100

    Time: 44 ms (11.13%), Space: 13.3 MB (63.72%) - LeetHub

--- 27-remove-element/27-remove-element.py
+++ 27-remove-element/27-remove-element.py
@@ -5,12 +5,6 @@ class Solution(object):
         :type val: int
         :rtype: int
         """
-        """l = 0
-        for r in range(len(nums)):
-            if val != nums:
-                nums.remove(val)
-            l += 1
-        return l"""
         i = 0
         for num in nums:
             if num != val:
