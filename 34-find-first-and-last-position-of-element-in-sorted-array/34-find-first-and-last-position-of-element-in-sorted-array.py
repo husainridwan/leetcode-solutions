f7a6e8af31a486ef1dc72bba264103c31edf1c55
Husain Ridwan <h.ridwan707@gmail.com>
2022-08-03 23:46:57 +0100

    Time: 127 ms (58.10%), Space: 15.4 MB (48.51%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,6 +1,6 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        first = self.binarySearch(nums, target, True)
+        """first = self.binarySearch(nums, target, True)
         last = self.binarySearch(nums, target, False)
         return [first, last]
         
@@ -19,6 +19,32 @@ class Solution:
                     r = m - 1
                 else:
                     l = m + 1
-        return i
+        return i"""
+        l = 0
+        r = len(nums)-1
+        res = [-1,-1]
+        while l<=r:
+            mid = (l+r)//2
+            if nums[mid] == target:
+                res[0]=mid #to store the position where target is found
+                r = mid -1 #to move the pointer to further left in order to find the first                      pos
+            elif nums[mid] > target:
+                r = mid -1
+            elif nums[mid] < target:
+                l = mid +1
+                
+        l = 0
+        r = len(nums)-1
         
+        while l<=r:
+            mid = (l+r)//2
+            if nums[mid] == target:
+                res[1] = mid #to store the position where target is found
+                l = mid +1 #to move the pointer to further right to find the first pos
+            elif nums[mid] > target:
+                r = mid -1
+            elif nums[mid] < target:
+                l = mid +1
+                
+        return res
         
