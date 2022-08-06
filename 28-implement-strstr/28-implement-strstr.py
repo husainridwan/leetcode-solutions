5440ceafaa8493e1fb841735d619d38959bea725
Husain Ridwan <h.ridwan707@gmail.com>
2022-08-06 12:54:02 +0100

    Time: 20 ms (77.19%), Space: 13.3 MB (95.65%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.py
+++ 28-implement-strstr/28-implement-strstr.py
@@ -7,8 +7,7 @@ class Solution(object):
         """
         if needle == "":
             return 0
-        
-        for i in range(len(haystack) +1 - len(needle)):
-            if haystack[i: i+len(needle)] == needle:
+        for i in range(len(haystack) - len(needle)+1):
+            if haystack[i:i+len(needle)] == needle:
                 return i
         return -1
