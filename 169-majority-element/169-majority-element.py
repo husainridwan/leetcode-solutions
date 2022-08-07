33027b3fc789f0f86a3bec25f386d9253bef546d
Husain Ridwan <h.ridwan707@gmail.com>
2022-08-07 23:38:07 +0100

    Time: 191 ms (81.37%), Space: 15.5 MB (35.08%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -1,7 +1,14 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        nums.sort()
-        return nums[len(nums)//2]
+        """nums.sort()
+        return nums[len(nums)//2]"""
+    
+        check = {}
+        for i in nums:
+            check[i] = check.get(i, 0) + 1
+            if check[i] > len(nums)//2:
+                return i
+        
 
             
         
