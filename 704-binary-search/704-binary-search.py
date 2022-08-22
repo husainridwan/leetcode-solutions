e8553749be95bc60c7ded0518ab3cc14e26a62ce
Husain Ridwan <h.ridwan707@gmail.com>
2022-08-22 23:31:04 +0100

    Time: 364 ms (52.88%), Space: 15.4 MB (73.46%) - LeetHub

--- 704-binary-search/704-binary-search.py
+++ 704-binary-search/704-binary-search.py
@@ -6,13 +6,14 @@ class Solution:
         if target not in nums:
             return -1
         while l < len(nums):
-            mid = (l+r)//2
+            mid = l + (r-l)//2
             if nums[mid] == target:
                 return mid
-            elif nums[mid] < target:
+            if target > nums[mid]:
                 l = mid + 1
             else:
                 r = mid - 1
+        return -1
 
             
         
