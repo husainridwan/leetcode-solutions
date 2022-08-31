f52afe793b7bb9be2bebbe120168bb34f3e38259
Husain Ridwan <h.ridwan707@gmail.com>
2022-08-31 23:35:46 +0100

    Time: 44 ms (70.77%), Space: 13.9 MB (53.74%) - LeetHub

--- 231-power-of-two/231-power-of-two.py
+++ 231-power-of-two/231-power-of-two.py
@@ -1,3 +1,10 @@
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
-        return n > 0 and (1 << 31) % n == 0
+        #return n > 0 and (1 << 31) % n == 0
+        if n == 1:
+            return True
+    
+        if n < 1:
+            return False
+    
+        return self.isPowerOfTwo(n/2)
