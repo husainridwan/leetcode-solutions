8fa8cb43342b1e458c1c3baa6c7f33307b0508c7
Husain Ridwan <h.ridwan707@gmail.com>
2022-09-05 22:33:38 +0100

    Time: 82 ms (48.62%), Space: 14 MB (12.07%) - LeetHub

---
+++ 441-arranging-coins/441-arranging-coins.py
@@ -0,0 +1,15 @@
+class Solution:
+    def arrangeCoins(self, n: int) -> int:
+        l, r = 1, n
+        res = 0
+        
+        while l <= r:
+            mid = (l + r)//2
+            #Using Gauss' formula for sum of integers
+            coins = (mid/2) * (mid + 1)
+            if coins > n:
+                r = mid - 1
+            else:
+                l = mid + 1
+                res = max(mid, res)
+        return res
