61fee2ca477e1e0112e48cef09c41123d7a2b21a
Husain Ridwan <h.ridwan707@gmail.com>
2022-09-08 23:54:14 +0100

    Time: 53 ms (97.19%), Space: 13.8 MB (74.08%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        maxSum = 0
+        for account in accounts:
+            curSum = sum(account)
+            if maxSum < curSum:
+                maxSum = curSum
+        return maxSum
