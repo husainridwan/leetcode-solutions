b5ad4c7497e5fe2086b60fac1a180c869cc3e386
Husain Ridwan <h.ridwan707@gmail.com>
2022-09-09 23:17:39 +0100

    Time: 970 ms (21.20%), Space: 13.7 MB (96.01%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -4,10 +4,4 @@ class Solution:
             return 0
         if n == 1:
             return 1
-        
-        a, b = 0, 1
-        for i in range(2, n+1):
-            c = a + b
-            a = b
-            b = c
-        return c
+        return self.fib(n-1) + self.fib(n-2)
