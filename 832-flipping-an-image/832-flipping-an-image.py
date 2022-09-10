0a72ca2ab1337e236947fe3e01adba3b3cf90b47
Husain Ridwan <h.ridwan707@gmail.com>
2022-09-10 23:51:26 +0100

    Time: 114 ms (7.43%), Space: 13.8 MB (66.22%) - LeetHub

--- 832-flipping-an-image/832-flipping-an-image.py
+++ 832-flipping-an-image/832-flipping-an-image.py
@@ -1,4 +1,5 @@
 class Solution:
     def flipAndInvertImage(self, image: List[List[int]]) -> List[List[int]]:
+        #Using XOR and reversing first
         return [[1^i for i in img[::-1]] for img in image]
     
