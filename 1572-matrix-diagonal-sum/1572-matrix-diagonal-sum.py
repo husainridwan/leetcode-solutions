c835f189a953589f91c84b658371ecf8b615bbbe
Husain Ridwan <h.ridwan707@gmail.com>
2022-09-11 23:26:34 +0100

    Time: 282 ms (5.21%), Space: 14.1 MB (57.03%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.py
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.py
@@ -4,9 +4,19 @@ class Solution:
         cols = len(mat[0])
         total = 0
         
-        for i in range(rows):
-            for j in range(cols):
-                if i == j or i+j == rows - 1:
-                    total += mat[i][j]
-        return total
+        #for i in range(rows):
+        #    for j in range(cols):
+        #        if i == j or i+j == rows - 1:
+        #            total += mat[i][j]
+        #return total
+
+        n = cols-1
         
+        for i in range(rows):
+		    # Add primary diagonal.
+            total += mat[i][i]
+			# Add the secondary but avoid the middle point that overlaps the primary.
+            if i != n-i:
+                total += mat[i][n-i]
+            
+        return total
