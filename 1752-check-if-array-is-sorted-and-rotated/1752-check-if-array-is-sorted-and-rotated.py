222458110c0d13fb70a93a7ac83d0b035b99298c
Husain Ridwan <h.ridwan707@gmail.com>
2022-09-13 23:21:13 +0100

    Time: 41 ms (83.89%), Space: 13.8 MB (58.61%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.py
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.py
@@ -1,12 +1,9 @@
 class Solution:
     def check(self, nums: List[int]) -> bool:
-        i = 0
-        while i<len(nums)-1:
-            if nums[i]>nums[i+1]: break    # used to find the rotated position
-            i+=1
+        #Sort the list
+        numSort = sorted(nums)
         
-        rotated = nums[i+1:]+nums[:i+1]
-        for i,e in enumerate(rotated):
-            if i<len(rotated)-1 and e>rotated[i+1]:   # check that rerotated array sorted or not
-                return False
-        return True 
+        for i in range(len(nums)):
+            if nums[i:] + nums[:i] == numSort:
+                return True
+        return False
