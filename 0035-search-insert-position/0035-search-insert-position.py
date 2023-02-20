215c57752ab0171168bee388f89baceff5d7a00c
Husain Ridwan <h.ridwan707@gmail.com>
2023-02-20 18:45:30 +0100

    Time: 40 ms (29.41%), Space: 14.2 MB (60.52%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        l, r = 0, len(nums)-1
+        
+        while l <= r:
+            mid = (l+r)//2
+            if target == nums[mid]:
+                return mid
+            elif target >= nums[mid]:
+                l = mid + 1
+            else:
+                r = mid - 1
+        return l 
