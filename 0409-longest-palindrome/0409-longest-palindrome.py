00bcc9aeb6fb7ea896a5c45a8974c24a395e5d03
Husain Ridwan <h.ridwan707@gmail.com>
2023-02-20 19:36:57 +0100

    Time: 15 ms (93.00%), Space: 13.3 MB (92.13%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        ss = set()
+        for letter in s:
+            if letter not in ss:
+                ss.add(letter)
+            else:
+                ss.remove(letter)
+        if len(ss) != 0:
+            return len(s) - len(ss) + 1
+        else:
+            return len(s)
