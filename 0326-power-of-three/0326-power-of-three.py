18f61f58a4d29a9a24ef16fbeb3bbb81eb7dd1ed
Husain Ridwan <h.ridwan707@gmail.com>
2023-02-22 22:25:52 +0100

    Time: 75 ms (79.16%), Space: 13.3 MB (91.88%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def isPowerOfThree(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n <= 0:
+            return False
+        idx = round(log(n, 3))
+        return 3 ** idx == n
