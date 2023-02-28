c2d31dad9b19a1eb01d47a55e0293940f1d9f93d
Husain Ridwan <h.ridwan707@gmail.com>
2023-02-28 13:35:19 +0100

    Time: 44 ms (68.29%), Space: 13.7 MB (38.41%) - LeetHub

---
+++ 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def findFinalValue(self, nums, original):
+        """
+        :type nums: List[int]
+        :type original: int
+        :rtype: int
+        """
+        nums = set(nums)
+        while original in nums:
+            original *= 2
+        return original
