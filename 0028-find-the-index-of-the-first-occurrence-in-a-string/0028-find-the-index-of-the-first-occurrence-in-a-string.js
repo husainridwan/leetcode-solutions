e4822a0574dca41c75f02aa2e10c4ee3fd415463
Husain Ridwan <h.ridwan707@gmail.com>
2023-07-17 23:51:31 +0100

    Time: 54 ms (81.41%), Space: 42.2 MB (27.89%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    if(needle == "") {
+        return 0;
+    }
+    if(haystack.includes(needle)) {
+        return haystack.indexOf(needle);
+    } else {
+        return -1;
+    }
+};
