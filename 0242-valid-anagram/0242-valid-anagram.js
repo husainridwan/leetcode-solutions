5c12efee29ed50fc23c371292260dfaa8de2bfe3
Husain Ridwan <h.ridwan707@gmail.com>
2023-07-24 22:36:56 +0100

    Time: 100 ms (35.83%), Space: 47.4 MB (39.00%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,12 +4,13 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    s = s.split('').sort();
-    t = t.split('').sort();
-    
     if(s.length !== t.length) {
         return false;
     }
+    
+    s = s.split('').sort();
+    t = t.split('').sort();
+    
     for(let i=0; i<s.length; i++) {
         if(s[i] !== t[i]) {
             return false;
