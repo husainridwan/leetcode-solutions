44d49554f6878b4a6e0e0a56b7a00be8034fbd13
Husain Ridwan <h.ridwan707@gmail.com>
2023-09-10 21:46:07 +0100

    Time: 54 ms, Memory: 42.4 MB - LeetHub

---
+++ swap-nodes-in-pairs/swap-nodes-in-pairs.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function(head) {
+    if (!head || !head.next) {
+        return head;
+    }
+    
+    let temp = head.next;
+    head.next = swapPairs(head.next.next);
+    temp.next = head;
+    
+    return temp;
+};
