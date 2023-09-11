3d4f718315b788a56f74e355fa6fff34fb41dd14
Husain Ridwan <h.ridwan707@gmail.com>
2023-09-11 23:55:17 +0100

    Time: 53 ms (55.61%), Space: 42.3 MB (9.29%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+    let result = new Array(digits.length-1).fill(1);
+    let intArr = BigInt(digits.join(""));
+    let intStr = String(intArr + BigInt(1));
+
+    for(let i=intStr.length-1; i >= 0; i--) {
+        result[i] = Number.parseInt(intStr[i]);
+    }
+    return result;  
+};
